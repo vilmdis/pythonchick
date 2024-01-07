8bf249d1dcfa0637d51ca91a039a65270d538e35
Sasha <shemakovsasha@gmail.com>
2024-01-07 15:29:32 +0200

    Work with files

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
-def func():
-    return None
+# def square(x):
+#     return x ** 2
+# old_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# new_list = list(map(square, old_list))
+# print(new_list)
 
-print('PRIVET MIR')
+# def valid(x):
+#     return x % 2 == 0
 
-if __name__ == '__main__':
-    print('OK')
+# list3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# result = list(filter(valid, list3))
+# print(result)
+
+# from functools import reduce
+
+# def add(x, y):
+#     return x + y
+
+# list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# res = reduce(add, list1)
+# print(res)
+
+# def len1(x, y):
+#     return x if len(x) > len(y) else y
+
+# list2 = ['apple', 'banana', 'watermalon', 'orange', 'carrot']
+# res1 = reduce(len1, list2)
+# print(res1)
+
+# res = lambda x, y: x * y
+# print(res(12, 23))
