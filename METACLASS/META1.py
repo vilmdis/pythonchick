594c9bfb2312e4181f18dcc3f2387a6fe37cac86
Sasha <shemakovsasha@gmail.com>
2024-05-14 23:32:06 +0300

    Change somesing in ООП5

---
+++ METACLASS/META1.py
@@ -0,0 +1,42 @@
+# import time
+
+# def count_num(n):
+#     if n == 0:
+#         return
+#     print(n)
+#     time.sleep(1)
+#     count_num(n - 1)
+
+# count_num(10)
+
+# def word_rev(x):
+#     if len(x) == 0:
+#         return x
+#     else:
+#         return word_rev(x[1:]) + x[0]
+
+# print(word_rev('hello'))
+
+# def factorial(n):
+#     if n == 1:
+#         return 1
+#     else:
+#         return n * factorial(n-1)
+
+# print(factorial(5))
+
+# def fibonachi(n):
+#     if n <= 1:
+#         return n
+#     else:
+#         return fibonachi(n-1) + fibonachi(n-2)
+
+# print(fibonachi(10))
+
+# def sum_of_num(n):
+#     if n == 0:
+#         return 0
+#     else:
+#         return n % 10 + sum_of_num(n // 10)
+
+# print(sum_of_num(1234))
--- ООП/ООП5.py
+++ ООП/ООП5.py
@@ -3,39 +3,41 @@ import re
 
 class Book:
 
-    def __init__(self, title, author, isbn, copies, total_copies):
+    def __init__(self, title:str, author:str, isbn:str, copies:int):
         self.title = title
         self.author = author
-        self.isbn = isbn
+        self.__isbn = isbn
         self.copies = copies
-        self.total_copies = total_copies
 
     def check_availability(self):
-        return self.copies > 0
-
-    def update_total_copies(self, num):
-        if num > 0:
-            self.total_copies = num
+        if bool(self.copies > 0):
+            return 'Ok'
         else:
-            return 'Total copies can`t be <0.'
+            return 'Not ok'
 
-    def update_copies(self, num):
+    def add_copies(self, num):
         if num > 0:
-            self.copies = num
+            self.copies += num
         else:
-            return 'Copies can`t be <0.'
+            print('Number can`t be <0.')
+
+    def withdraw_copies(self, num):
+        if num > 0 and num < self.copies:
+            self.copies -= num
+        else:
+            print('Copies can`t be <0.')
+
+    def get_isbn(self):
+        return self.__isbn
 
     def validate_isbn(self):
         isbn = re.compile(r'^978-\d{1}+-\d{4}-\d{4}-\d{1}$')
-        return bool(isbn.match(self.isbn))
-
-    def get_isbn(self):
-        return self.isbn
+        return bool(isbn.match(self.__isbn))
 
 
 class User:
 
-    def __init__(self, name, user_id):
+    def __init__(self, name:str, user_id:str):
         self._name = name
         self._user_id = user_id
 
@@ -52,37 +54,32 @@ class Customer(User):
         super().__init__(name, user_id)
         self.borrowed_books = []
 
-    def get_name(self):
-        return super().get_name()
-
-    def get_id(self):
-        return super().get_id()
-
-    def take_books(self, book):
-        if book.check_availability():
-            book.update_copies(book.copies - 1)
+    def take_books(self, book, num):
+        if book.copies >= num and num > 0:
+            book.copies -= num
             self.borrowed_books.append(book)
             return f'{self.get_name()} has borrowed {book.title}'
         else:
             return 'Sorry, we don`t have this book.'
 
-    def give_away_books(self, book):
+    def give_away_books(self, book, num):
         if book in self.borrowed_books:
-            book.update_copies(book.copies + 1)
-            return self.borrowed_books.pop(book)
+            book.copies += num
+            self.borrowed_books.remove(book)
+            return f'{self.get_name()} give away {book.title}'
+        elif num <= 0:
+            return 'Invalid number.'
         else:
             return 'You don`t have this book.'
 
 
-class Employee(User):
+class Employee(User, Book):
 
-    def __init__(self, name, salary, library):
+    def __init__(self, name, salary:int, library:list):
         super().__init__(name, user_id=None)
-        self.__salary = int(salary)
+        self.__salary = salary
         self.library = library
-
-    def get_name(self):
-        return super().get_name()
+        self.total_copies = 0
 
     def get_salary(self):
         return f'Your salary is {self.__salary}$.'
@@ -94,7 +91,7 @@ class Employee(User):
         else:
             return 'Invalid number.'
 
-    def withdraw_salary(self, num):
+    def reduce_salary(self, num):
         if num > 0 and num <= self.__salary:
             self.__salary -= num
             return f'Withdrawn {num}$ from salary.'
@@ -102,14 +99,14 @@ class Employee(User):
             return 'Invalid number.'
 
     def add_books(self, book):
-        book.update_total_copies(book.total_copies + book.copies)
         self.library.books.append(book)
+        self.total_copies += 1
         return f'{book.title} added to the library.'
 
     def del_books(self, book):
         if book in self.library.books:
-            book.update_total_copies(book.total_copies - book.copies)
             self.library.books.remove(book)
+            self.total_copies += 1
             return f'{book.title} removed from the library.'
         else:
             return 'This book isn`t in the library.'
@@ -127,7 +124,7 @@ class Library:
     def show_all_books(self):
         return self.books
 
-    def verify_user(self, user):
+    def register_user(self, user):
         self.users.append(user)
 
     def search_books(self):
@@ -139,8 +136,8 @@ class Library:
 
 library = Library()
 
-book1 = Book("Harry Potter", "Rowling", "978-5-4675-2345-7", 5, 10)
-book2 = Book("The Great Gatsby", "Scott Fitzgerald", "978-0-7432-7356-5", 3, 11)
+book1 = Book("Harry Potter", "Rowling", "978-5-4675-2345-7", 5)
+book2 = Book("The Great Gatsby", "Scott Fitzgerald", "978-0-7432-7356-5", 3)
 
 customer1 = Customer("Vlad", 135235)
 employee1 = Employee("Vadim", 3000, library)
@@ -148,17 +145,26 @@ employee1 = Employee("Vadim", 3000, library)
 employee1.add_books(book1)
 employee1.add_books(book2)
 
-library.verify_user(customer1)
-library.verify_user(employee1)
+library.register_user(customer1)
+library.register_user(employee1)
+
+print(book1.copies)
+print(book2.copies)
+
+book1.withdraw_copies(10)
+book2.add_copies(10)
 
 print(book1.check_availability())
-book1.update_copies(4)
-print(book1.total_copies)
+
+print(employee1.total_copies)
 print(book1.validate_isbn())
 print(book1.get_isbn())
 
 print(employee1.add_salary(500))
 
-print(customer1.take_books(book1))
+print(customer1.take_books(book1, 5))
 print(employee1.del_books(book2))
-print(customer1.give_away_books(book2))
+print(customer1.give_away_books(book1, 7))
+
+print(book1.copies)
+print(book2.copies)
